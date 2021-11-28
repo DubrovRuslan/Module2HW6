d1fa4413cc620c7439721cb0ace7db3b9f9b43c2
Dubrov Ruslan <Ruslan Dubrov>
2021-11-28 21:35:33 +0200

    created Providers

---
+++ Module2HW6/Providers/Abstractions/IElectricalApplianceProvider.cs
@@ -0,0 +1,10 @@
+﻿using Module2HW6.Models.Abstractions;
+
+namespace Module2HW6.Providers.Abstractions
+{
+    public interface IElectricalApplianceProvider
+    {
+        ElectricalAppliance[] GetAllAppliances();
+        bool AddNewElectricalAppliance(ElectricalAppliance electricalAppliance);
+    }
+}
---
+++ Module2HW6/Providers/ElectricalApplianceProvider.cs
@@ -0,0 +1,26 @@
+﻿using Module2HW6.Helpers;
+using Module2HW6.Models.Abstractions;
+using Module2HW6.Providers.Abstractions;
+
+namespace Module2HW6.Providers
+{
+    public class ElectricalApplianceProvider : IElectricalApplianceProvider
+    {
+        private ElectricalAppliance[] _electricalAppliances;
+
+        public ElectricalAppliance[] GetAllAppliances()
+        {
+            return _electricalAppliances;
+        }
+
+        public bool AddNewElectricalAppliance(ElectricalAppliance electricalAppliance)
+        {
+            if (electricalAppliance == null)
+            {
+                return false;
+            }
+
+            return ArrayHelper.AddElementToArray(ref _electricalAppliances, electricalAppliance);
+        }
+    }
+}
