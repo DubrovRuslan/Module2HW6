64cc08129b885074bbc67e024475ddddaf99f8c1
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-12 19:11:20 +0300

    Electrical Appliance Abstractions done

---
+++ Models/Abstractions/ElectricalAppliance.cs
@@ -0,0 +1,12 @@
+﻿namespace Module2HW6.Models.Abstractions
+{
+    public abstract class ElectricalAppliance
+    {
+        public string Name { get; set; }
+
+        public int Power { get; set; }
+
+        public bool IsEnable { get; set; }
+        public abstract ElectricalApplianceType ElectricalApplianceType { get; }
+    }
+}
